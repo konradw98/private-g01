ba769e778b7ff5233ab8726282fef4cd7d8f8f01
konrad <konradwodnicki98@gmail.com>
2021-04-25 22:12:50 +0100

    change to new version of scrypt

--- src/test/sql/scryptDb.sql
+++ src/test/sql/scryptDb.sql
@@ -1,24 +1,24 @@
-CREATE TABLE sport (
-	sid INT NOT NULL PRIMARY KEY,
-	name VARCHAR(50) NOT NULL,
-	description VARCHAR(255) NOT NULL
+CREATE TABLE sports (
+    sid SERIAL UNIQUE,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE users(
-	uid INT NOT NULL PRIMARY KEY,
-	email VARCHAR(50) NOT NULL,
-	name VARCHAR(50) NOT NULL );
+    uid SERIAL UNIQUE,
+    email VARCHAR(50) NOT NULL,
+    name VARCHAR(50) NOT NULL );
 
-CREATE TABLE route(
-	rid INT NOT NULL PRIMARY KEY,
-	start_location VARCHAR(50) NOT NULL,
-	end_location VARCHAR(50) NOT NULL,
-	distance INT NOT NULL);
+CREATE TABLE routes(
+        rid SERIAL UNIQUE,
+        start_location VARCHAR(50) NOT NULL,
+        end_location VARCHAR(50) NOT NULL,
+        distance INT NOT NULL);
 
-CREATE TABLE activity (
-	aid INT PRIMARY KEY,
-	date date NOT NULL,
-	duration_time INT NOT NULL,
-	sid INT REFERENCES sport(sid)NOT NULL,
-	uid INT REFERENCES users(uid) NOT NULL,
-	rid INT REFERENCES route(rid));
+CREATE TABLE activities (
+aid SERIAL UNIQUE,
+date date NOT NULL,
+duration_time TIME NOT NULL,
+sid INT REFERENCES sports(sid)NOT NULL,
+uid INT REFERENCES users(uid) NOT NULL,
+rid INT REFERENCES routes(rid));
