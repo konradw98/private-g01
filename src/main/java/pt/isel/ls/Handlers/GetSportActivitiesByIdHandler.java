f0d4af91572cb5cbd74f88d015f7798544d242d3
konrad <konradwodnicki98@gmail.com>
2021-05-04 11:12:56 +0100

    refactor GetSportActivitiesHandler,GetUserActivitiesByIdHandler andd GetUserActivitiesHandler

--- src/main/java/pt/isel/ls/Handlers/GetSportActivitiesByIdHandler.java
+++ src/main/java/pt/isel/ls/Handlers/GetSportActivitiesByIdHandler.java
@@ -1,38 +1,57 @@
 package pt.isel.ls.Handlers;
 
 import pt.isel.ls.CommandRequest;
+import pt.isel.ls.CommandResults.CommandResult;
+import pt.isel.ls.CommandResults.GetActivitiesResult;
+import pt.isel.ls.CommandResults.WrongParametersResult;
+import pt.isel.ls.Models.Activity;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Optional;
 
 public class GetSportActivitiesByIdHandler implements CommandHandler {
 
     @Override
-    public Optional<CommandResult> execute(CommandRequest commandRequest) throws SQLException {
+    public CommandResult execute(CommandRequest commandRequest) throws SQLException {
         Connection conn = commandRequest.getDataSource().getConnection();
         ArrayList<String> parameters = commandRequest.getPathParameters();
-        int sid = Integer.parseInt(parameters.get(0));
-        int aid = Integer.parseInt(parameters.get(1));
+        int sidParam = Integer.parseInt(parameters.get(0));
+        int aidParam = Integer.parseInt(parameters.get(1));
 
-        String wrongParameters = checkParameters(sid, aid, conn);
+        String wrongParameters = checkParameters(sidParam, aidParam, conn);
         if (!wrongParameters.equals("")) {
             conn.close();
             System.out.println("Wrong parameter:" + wrongParameters);
-            return Optional.empty();
+            return new WrongParametersResult();
         }
 
         String sql = "SELECT * FROM activities WHERE sid=? AND aid=?";
         PreparedStatement pstmt = conn.prepareStatement(sql);
-        pstmt.setInt(1, sid);
-        pstmt.setInt(2, aid);
-        Optional<CommandResult> optional = Optional.of(new CommandResult(pstmt.executeQuery()));
+        pstmt.setInt(1, sidParam);
+        pstmt.setInt(2, aidParam);
+        ResultSet resultSet = pstmt.executeQuery();
         conn.close();
 
-        return optional;
+        int aid, sid, rid, uid;
+        Date date;
+        Time duration_time;
+        Activity activity;
+        ArrayList<Activity> activities = new ArrayList<>();
+
+        while (resultSet.next()) {
+            aid = resultSet.getInt("aid");
+            date = resultSet.getDate("date");
+            duration_time = resultSet.getTime("duration_time");
+            sid = resultSet.getInt("sid");
+            uid = resultSet.getInt("uid");
+            rid = resultSet.getInt("rid");
+            activity = new Activity(aid, date, duration_time, sid, uid, rid);
+            activities.add(activity);
+        }
+        if (activities.size() == 0) {
+            return new WrongParametersResult();
+        } else return new GetActivitiesResult(activities);
     }
 
     private String checkParameters(int sid, int aid, Connection conn) throws SQLException {
--- src/main/java/pt/isel/ls/Handlers/GetUserActivitiesByIdHandler.java
+++ src/main/java/pt/isel/ls/Handlers/GetUserActivitiesByIdHandler.java
@@ -1,39 +1,58 @@
 package pt.isel.ls.Handlers;
 
 import pt.isel.ls.CommandRequest;
+import pt.isel.ls.CommandResults.CommandResult;
+import pt.isel.ls.CommandResults.GetActivitiesResult;
+import pt.isel.ls.CommandResults.WrongParametersResult;
+import pt.isel.ls.Models.Activity;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Optional;
 
 public class GetUserActivitiesByIdHandler implements CommandHandler {
 
     @Override
-    public Optional<CommandResult> execute(CommandRequest commandRequest) throws SQLException {
+    public CommandResult execute(CommandRequest commandRequest) throws SQLException {
         Connection conn = commandRequest.getDataSource().getConnection();
         ArrayList<String> parameters = commandRequest.getParameters();
 
-        int uid = Integer.parseInt(parameters.get(0));
-        int aid = Integer.parseInt(parameters.get(1));
-        String wrongParameters = checkParameters(uid, aid, conn);
+        int uidParam = Integer.parseInt(parameters.get(0));
+        int aidParam = Integer.parseInt(parameters.get(1));
+        String wrongParameters = checkParameters(uidParam, aidParam, conn);
 
         if (!wrongParameters.equals("")) {
             conn.close();
             System.out.println("Wrong parameter:" + wrongParameters);
-            return Optional.empty();
+            return new WrongParametersResult();
         }
 
         String sql = "SELECT * FROM activities WHERE uid=? AND aid=?";
         PreparedStatement pstmt = conn.prepareStatement(sql);
-        pstmt.setInt(1, uid);
-        pstmt.setInt(2, aid);
-        Optional<CommandResult> optional = Optional.of(new CommandResult(pstmt.executeQuery()));
+        pstmt.setInt(1, uidParam);
+        pstmt.setInt(2, aidParam);
+        ResultSet resultSet = pstmt.executeQuery();
         conn.close();
 
-        return optional;
+        int aid, sid, rid, uid;
+        Date date;
+        Time duration_time;
+        Activity activity;
+        ArrayList<Activity> activities = new ArrayList<>();
+
+        while (resultSet.next()) {
+            aid = resultSet.getInt("aid");
+            date = resultSet.getDate("date");
+            duration_time = resultSet.getTime("duration_time");
+            sid = resultSet.getInt("sid");
+            uid = resultSet.getInt("uid");
+            rid = resultSet.getInt("rid");
+            activity = new Activity(aid, date, duration_time, sid, uid, rid);
+            activities.add(activity);
+        }
+        if (activities.size() == 0) {
+            return new WrongParametersResult();
+        } else return new GetActivitiesResult(activities);
     }
 
     private String checkParameters(int uid, int aid, Connection conn) throws SQLException {
