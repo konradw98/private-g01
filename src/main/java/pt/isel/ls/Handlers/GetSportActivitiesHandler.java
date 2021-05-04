db903cb96b0e907975ca84aecae8f8351b37760f
konrad <konradwodnicki98@gmail.com>
2021-05-04 11:03:00 +0100

    add acvitity model, getActivitieResult and refactor GetSportActivitiesHandler

--- src/main/java/pt/isel/ls/Handlers/GetSportActivitiesHandler.java
+++ src/main/java/pt/isel/ls/Handlers/GetSportActivitiesHandler.java
@@ -1,24 +1,47 @@
 package pt.isel.ls.Handlers;
 
 import pt.isel.ls.CommandRequest;
+import pt.isel.ls.CommandResults.CommandResult;
+import pt.isel.ls.CommandResults.GetActivitiesResult;
+import pt.isel.ls.CommandResults.GetUsersResult;
+import pt.isel.ls.CommandResults.WrongParametersResult;
+import pt.isel.ls.Models.Activity;
+import pt.isel.ls.Models.User;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Optional;
 
 public class GetSportActivitiesHandler implements CommandHandler {
     @Override
-    public Optional<CommandResult> execute(CommandRequest commandRequest) throws SQLException {
+    public CommandResult execute(CommandRequest commandRequest) throws SQLException {
         Connection conn = commandRequest.getDataSource().getConnection();
         ArrayList<String> parameters = commandRequest.getPathParameters();
 
         String sql = "SELECT * FROM activities WHERE sid=?";
         PreparedStatement pstmt = conn.prepareStatement(sql);
         pstmt.setInt(1, Integer.parseInt(parameters.get(0)));
-        Optional<CommandResult> optional = Optional.of(new CommandResult(pstmt.executeQuery()));
+        ResultSet resultSet=pstmt.executeQuery();
         conn.close();
-        return optional;
+
+        int aid, sid,rid,uid;
+        Date date;
+        Time duration_time;
+        Activity activity;
+        ArrayList<Activity> activities = new ArrayList<>();
+
+        while (resultSet.next()) {
+            aid=resultSet.getInt("aid");
+            date=resultSet.getDate("date");
+            duration_time=resultSet.getTime("duration_time");
+            sid=resultSet.getInt("sid");
+            uid = resultSet.getInt("uid");
+            rid=resultSet.getInt("rid");
+            activity=new Activity(aid,date,duration_time,sid,uid,rid);
+            activities.add(activity);
+        }
+        if (activities.size() == 0) {
+            return new WrongParametersResult();
+        } else return new GetActivitiesResult(activities);
     }
 }
