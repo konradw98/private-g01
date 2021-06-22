b0b4c4ed69df7ea7b5a296d56e1e2c151f5ea4a1
konrad <konradwodnicki98@gmail.com>
2021-06-22 19:30:53 +0100

    update addData

--- src/test/sql/addData.sql
+++ src/test/sql/addData.sql
@@ -1,3 +1,27 @@
-insert into courses(name) values ('LEIC');
-insert into students(course, number, name) values (1, 12345, 'Alice');
-insert into students(course, number, name) select cid as course, 12346 as number, 'Bob' as name from courses where name = 'LEIC'
+INSERT INTO users(email,name) VALUES('kamil123@o2.pl','Kamil Nowakowski');
+INSERT INTO users(email,name) VALUES('piotr123@o2.pl','Kamil Lewandowski');
+INSERT INTO users(email,name) VALUES('patryk123@o2.pl','Patryk Baranowski');
+INSERT INTO users(email,name) VALUES('mateusz23@o2.pl','Mateusz Kowalski');
+INSERT INTO users(email,name) VALUES('sebastian123@gmai.com','Sebastian Nowak');
+
+INSERT INTO sports(name, description) VALUES('running', 'Running has been described as the worlds most accessible sport.');
+INSERT INTO sports(name, description) VALUES('cycling', 'Cycle sport is competitive physical activity using bicycles.');
+INSERT INTO sports(name, description) VALUES('skating', 'Sport on ice.');
+INSERT INTO sports(name, description) VALUES('speed skating', 'Speed sport on ice.');
+INSERT INTO sports(name, description) VALUES('squash', 'Squash is a racket and ball sport played by two players in a four-walled court with a small, hollow rubber ball.');
+INSERT INTO sports(name, description) VALUES('tennis', 'Tennis is a racket sport that can be played individually against a single opponent (singles) or between two teams of two players each (doubles).');
+INSERT INTO sports(name, description) VALUES('windsurfing', 'Windsurfing, also called boardsailing, sport that combines aspects of sailing and surfing on a one-person craft called a sailboard');
+INSERT INTO sports(name, description) VALUES('mountains climbing', 'The sport of attaining, or attempting to attain, high points in mountainous regions, mainly for the pleasure of the climb. .');
+INSERT INTO sports(name, description) VALUES('horse racing', 'Sport of running horses at speed, mainly Thoroughbreds with a rider astride or Standardbreds with the horse pulling a conveyance with a driver. ');
+INSERT INTO sports(name, description) VALUES('sailing', 'Sailing specifically refers to the sport of using wind to power sails and propel the boat forward (rather than engines).');
+
+INSERT INTO (start_location, end_location, distance) VALUES('Bairro Alto','Alameda',4);
+INSERT INTO (start_location, end_location, distance) VALUES('ISEL','Alameda',3);
+INSERT INTO (start_location, end_location, distance) VALUES('Bairro Alto','Cais do Sodre',1);
+INSERT INTO (start_location, end_location, distance) VALUES('Belem','Cais do Sodre',7);
+INSERT INTO (start_location, end_location, distance) VALUES('Bairro Alto','Belem',8);
+INSERT INTO (start_location, end_location, distance) VALUES('Bairro Alto','Pontinha',10);
+INSERT INTO (start_location, end_location, distance) VALUES('Bairro Alto','Carcavelos',18);
+INSERT INTO (start_location, end_location, distance) VALUES('Carcavelos','Belem',11);
+INSERT INTO (start_location, end_location, distance) VALUES('Carcavelos','Cascais',8);
+INSERT INTO (start_location, end_location, distance) VALUES('Lisboa','Cascais',30);
