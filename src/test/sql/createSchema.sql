06cbc810a929a9b984eb49fb8e5886d3bce3a0a4
konrad <konradwodnicki98@gmail.com>
2021-06-22 17:31:34 +0100

    update sql script

--- src/test/sql/createSchema.sql
+++ src/test/sql/createSchema.sql
@@ -1,13 +1,30 @@
-drop table if exists students;
-drop table if exists courses;
+drop table if exists sports;
+drop table if exists users;
+drop table if exists activities;
+drop table if exists routes;
 
-create table courses (
-  cid serial primary key,
-  name varchar(80)
+CREATE TABLE sports (
+    sid SERIAL UNIQUE,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(255) NOT NULL
 );
 
-create table students (
-  number int primary key,
-  name varchar(80),
-  course int references courses(cid)
-);
+CREATE TABLE users(
+    uid SERIAL UNIQUE,
+    email VARCHAR(50)  UNIQUE NOT NULL,
+    name VARCHAR(50) NOT NULL );
+
+CREATE TABLE routes(
+    rid SERIAL UNIQUE,
+    start_location VARCHAR(50) NOT NULL,
+    end_location VARCHAR(50) NOT NULL,
+    distance DECIMAL NOT NULL);
+
+CREATE TABLE activities (
+    aid SERIAL UNIQUE,
+    date date NOT NULL,
+    duration_time TIME NOT NULL,
+    sid INT REFERENCES sports(sid)NOT NULL,
+    uid INT REFERENCES users(uid) NOT NULL,
+    rid INT REFERENCES routes(rid),
+    timestamp TIMESTAMP);
